f8fecf7059643d76dcfa61fb4a2c76d32f5d3248
Ilya Morozov <balu126@yandex.ru>
2022-08-21 23:24:31 +0300

    Wrote the program an even number or not

---
+++ task_6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA%2 == 0)
+{
+    Console.WriteLine($"Число {numberA} чётное");
+}
+else
+{
+    Console.WriteLine($"Число {numberA} нечётное");
+}

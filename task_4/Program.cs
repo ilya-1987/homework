afed9cdf1ee2927a118018c3049e8163c0cf95a5
Ilya Morozov <balu126@yandex.ru>
2022-08-21 23:09:01 +0300

    I wrote a program for comparing three numbers and choosing the maximum

---
+++ task_4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите три числа:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+int numberC = Convert.ToInt32(Console.ReadLine());
+int max;
+if (numberA > numberB)
+{
+    max = numberA;
+}
+else if (numberB > numberC)
+{
+    max = numberB;
+}
+else
+{
+    max = numberC;
+}
+
+Console.WriteLine($"Число {max} максимальное из введенных чисел");

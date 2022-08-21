af0c02de2ebe2e11e13726814e229df0c926d957
Ilya Morozov <balu126@yandex.ru>
2022-08-21 22:48:59 +0300

    I wrote a program for comparing two numbers

---
+++ task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿  Console.WriteLine("Введите два числа");
+        int numberA = Convert.ToInt32(Console.ReadLine());
+        int numberB = Convert.ToInt32(Console.ReadLine());
+        if (numberA > numberB)
+            { 
+            Console.WriteLine($"Число {numberA} большеe, а число {numberB} меньшее");
+            }
+        else 
+        {
+               Console.WriteLine($"Число {numberB} большее, а число {numberA} меньшее");
+            
+        }
+

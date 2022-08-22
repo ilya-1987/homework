c6207b583054584c1f977c8bff50b28b034785be
Ilya Morozov <balu126@yandex.ru>
2022-08-22 20:17:49 +0300

    output of all even numbers in the range from 1 to n

---
+++ task_8/Program.cs
@@ -0,0 +1,11 @@
+﻿ Console.Write("Введите число ");
+        int numberA = Convert.ToInt32(Console.ReadLine());
+        
+    Console.Write($"Все четные числа в диапазоне от 1 до {numberA}: ");
+        for (int i = 1; i <= numberA; i++ )
+        {
+        if (i % 2 == 0)
+        {
+           Console.Write(i + " ");
+        }
+        }

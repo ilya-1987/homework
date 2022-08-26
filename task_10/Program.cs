dbd8743644872ea593d1dd84872cbc5f392b9471
Ilya Morozov <balu126@yandex.ru>
2022-08-26 22:11:19 +0300

    A program showing the second digit of a three digit number

---
+++ task_10/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите трех значное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0)
+{
+    number = number * (-1);
+}
+int result = (number / 10) % 10;
+Console.WriteLine($"Вторая цифра числа {result}");

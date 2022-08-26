650df188c8c55b1a01124399c483faa569b99a52
Ilya Morozov <balu126@yandex.ru>
2022-08-26 23:39:32 +0300

    Definition of a working day or a day off by the number of the day of the week.

---
+++ task_15/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+
+Console.Write ("Введите номер дня недели: ");
+string GetDayWeek(int number)
+{
+    if (number < 1 | number > 7 )
+    {
+        return "Введено не коректное число.";
+    }
+    else 
+    
+        if (number > 5)
+        {
+            return "Выходной день";    
+        }
+               
+    
+    else
+        {
+       return "Рабочий день";
+        }
+        
+}
+
+int day = Convert.ToInt32(Console.ReadLine());
+System.Console.Write(GetDayWeek(day));

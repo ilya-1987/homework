863b4f4670fa2ae2e528a3312fd0ecc8de91dff4
Ilya Morozov <balu126@yandex.ru>
2022-09-04 23:23:35 +0300

    Raising a number to a natural power

---
+++ task_25/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.WriteLine("Введите число и степень в которую нужно возвести число");
+int num = Convert.ToInt32(Console.ReadLine()); // Число которое необходимо возвести в степень
+int x = Convert.ToInt32(Console.ReadLine()); // Степень в которую необходимо возвести число
+int Pow (int number, int rank){        // Функция возводящая число в степень
+    if (rank == 0) {
+        return 1;
+    }
+
+    int result = 1;
+
+for (int i = 0; i < rank; i++){
+    result *= number;
+}
+return result;
+}
+
+System.Console.Write($"Число {num} в степени {x} будет равно {Pow(num, x)}");

87380630cbd684482ecdfead057c78011c34b48c
Ilya Morozov <balu126@yandex.ru>
2022-09-04 23:59:36 +0300

    Program for output of an array of eight elements

---
+++ task_29/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int [] GetArray (int size = 8){
+    int [] arr = new int [size];
+
+    for ( int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(10);
+    }
+return arr;
+}
+
+foreach (var item in GetArray()){
+    Console.WriteLine(item);
+}

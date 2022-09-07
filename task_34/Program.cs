1ab7efba8b7ba91a3886a6075ae36393f5ead498
Ilya Morozov <balu126@yandex.ru>
2022-09-07 22:52:33 +0300

    Counting the number of even array elements

---
+++ task_34/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте массив заполненный случайными положительными 
+// трёхзначными числами. Напишите программу, которая покажет 
+// количество чётных чисел в массиве.
+
+int [] GetArray (int size, int leftRange, int rightRange)
+{
+    int [] array = new int [size];
+    Random rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+    array [i] = rand.Next(leftRange, rightRange+1);
+    }
+    return array;
+}
+   
+int GetEventCout (int [] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array [i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int [] arr = GetArray (12, 100, 999);
+
+Console.WriteLine(string.Join("," , arr));
+Console.WriteLine($"Количество четных элементов массива равно {GetEventCout (arr)}");

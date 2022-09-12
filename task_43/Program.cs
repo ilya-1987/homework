56a779d17d0b1768ebeaaf325b647b5a907dc838
Ilya Morozov <balu126@yandex.ru>
2022-09-12 18:50:29 +0300

    The program for finding the coordinates of the intersection point of two straight lines

---
+++ task_43/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+System.Console.WriteLine("Введите координаты точек двух прямых через пробел:");
+string [] stringArray = Console.ReadLine().Split(' ');
+ 
+double b1 =  double.Parse(stringArray[0]);
+double k1 =  double.Parse(stringArray[1]);
+ 
+double b2 =  double.Parse(stringArray[2]);
+double k2 =  double.Parse(stringArray[3]);
+ 
+double [] GetPoint (double b1, double k1, double b2, double k2){
+    double [] result = new double[2];
+    result[0] = (b2-b1) / (k1-k2);
+    result[1] = k1 * result[0] + b1;
+ 
+    return result;
+}
+ 
+System.Console.WriteLine("Координата точки пересечения двух прямых: ");
+System.Console.WriteLine(string.Join(" ", GetPoint(b1,k1,b2,k2)));

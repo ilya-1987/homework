120046601051a50b9c95e76a3c838ecbd8c3c3a7
Ilya Morozov <balu126@yandex.ru>
2022-09-12 18:46:04 +0300

    A program for counting positive numbers in an array

---
+++ task_41/Program.cs
@@ -0,0 +1,24 @@
+﻿int [] GetArrayFromString (string stringArray){  // Функция организации массива
+    string [] nums = stringArray.Split(' ');
+    int [] result = new int [nums.Length];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result [i] = Convert.ToInt32(nums[i]);
+    }
+    return result;
+}
+ 
+int CountPositiveNumbers(int [] arr){ // Функция проверки количества положительных чисел в массиве
+    int count = 0;
+    foreach (var item in arr)
+    {
+        if (item > 0){
+            count++;
+        }
+    }
+    return count;
+}
+ 
+System.Console.WriteLine("Введите числа через пробел: ");
+int [] numbers = GetArrayFromString(Console.ReadLine());
+System.Console.WriteLine($"Количество положительных чисел в массиве: {CountPositiveNumbers(numbers)}");

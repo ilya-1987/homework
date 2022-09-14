999277808186799ae19beb072508cec7d2a33dbf
Ilya Morozov <balu126@yandex.ru>
2022-09-14 23:36:33 +0300

    The output of a two-dimensional array is given by random numbers

---
+++ task_47/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+ 
+int rows = 3;
+int columns = 4;
+ 
+double [,] GetArray (int m, int n){
+    var result = new double [m, n];
+    for (int i = 0; i < m; m++)
+    {
+        for (int j = 0; j < n; n++ )
+        {
+          result [i, j] = new Random().NextDouble();
+        }
+    }
+ 
+    return result;
+}
+ 
+void PrintArray (double[,] arr){
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++ )
+        {
+         Console.Write($"{arr[i, j]} ");
+        }
+    System.Console.WriteLine();
+    }
+}
+
+PrintArray(GetArray(rows, columns));

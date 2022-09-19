aa09dc30d0c93be668201aed894a80793c862a06
Ilya Morozov <balu126@yandex.ru>
2022-09-19 23:29:18 +0300

    A program for finding the sum of natural numbers in the range from M to N.

---
+++ task_66/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int m = InputInt("Введите первое натуральное число: ");
+int n = InputInt("Введите второе натуральное число: ");
+if (m < 1 | n < 1 )
+{
+    Console.WriteLine("Вы ввели не натуральные числа.");
+}
+else
+{
+    Console.WriteLine($"Сумма всех натуральных чисел в промежутке от {m} до {n} равна {CountNaturalSum(m, n)}");
+}
+
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int CountNaturalSum(int m, int n)
+{
+    if (m == n)
+    return n;
+    return n + CountNaturalSum(m, n - 1);
+}
